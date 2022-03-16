0514d6bdfa15df8ea7f54055c5786e6198641cdc
Trương Văn Thành <45113398+TruongThanh207@users.noreply.github.com>
2022-03-16 18:01:23 +0700

    [v1] clone web html css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,19 @@
-window.addEventListener('resize', function(event) {
+var root = document.querySelector(':root');
+window.addEventListener('resize', function() {
     var heightSlide = $('#slide-show')[0].children[0].childNodes[1].clientHeight
 
     // js
 
-    var root = document.querySelector(':root');
     root.style.setProperty('--height-slide', heightSlide + 'px'); 
 }, true);
 
+window.addEventListener('scroll', function(){
+    var header = document.getElementsByClassName('section-header')[0];
+    if(this.window.scrollY > 300){
+        header.classList.add('fixed')
+        root.style.setProperty('--height-header', '70px');
+    }else{
+        header.classList.remove('fixed')
+        root.removeAttribute('style')
+    }
+})
