4b03448d3e5ec351dddd6809beba69e7685adc02
Trương Văn Thành <45113398+TruongThanh207@users.noreply.github.com>
2022-03-25 23:50:18 +0700

    [v1] 95% layout

---
+++ assets/js/map.js
@@ -0,0 +1,16 @@
+function initMap() {
+    // The location of Uluru
+    const uluru = { lat: 20.925444, lng: 105.666815 };
+    // The map, centered at Uluru
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 4,
+      center: uluru,
+    });
+    // The marker, positioned at Uluru
+    const marker = new google.maps.Marker({
+      position: uluru,
+      map: map,
+    });
+}
+
+console.log(1)
